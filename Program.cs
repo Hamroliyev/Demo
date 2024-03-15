bd8719f84e80efb19b4272bd646449a2a09a38e0
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-03-16 01:12:14 +0500

    Refactor Program cs

--- Program.cs
+++ Program.cs
@@ -7,20 +7,49 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Order order = new Order();
-             Sea sea = new Sea();
-             Ground ground = new Ground();
+            Sea sea = new Sea();
+            Ground ground = new Ground();
             Air air = new Air();
-            order.SetShippingType(ground);
-            Console.WriteLine(order.GetShippingCost());
 
-            order.SetShippingType(sea);
-            Console.WriteLine(order.GetShippingCost());
+            do
+            {
+                PrintMenu();
+                Console.Write("Enter choice : ");
+                string userChoise = Console.ReadLine();
 
-            order.SetShippingType(air);
-            order.SetShippingType(air);
-            Console.WriteLine(order.GetShippingCost());
-
+                switch (userChoise)
+                {
+                    case "1":
+                        {
+                            order.SetShippingType(ground);
+                            Console.WriteLine(order.GetShippingCost());
+                            Console.WriteLine();
+                        }
+                        break;
+                    case "2":
+                        {
+                            order.SetShippingType(air);
+                            Console.WriteLine(order.GetShippingCost());
+                            Console.WriteLine();
+                        }
+                        break;
+                    case "3":
+                        {
+                            order.SetShippingType(sea);
+                            Console.WriteLine(order.GetShippingCost());
+                            Console.WriteLine();
+                        }
+                        break;
+                }
+            } while (true);
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("1.Air");
+            Console.WriteLine("2.Ground");
+            Console.WriteLine("3.Sea");
         }
     }
 }
